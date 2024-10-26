03fd1579ad2eed680821b5e29dc738baf89b654d
Dean <deanhuang@elstech.com.tw>
2024-10-26 11:38:02 +0800

    feat: 目錄滾動效果

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,22 @@ import 'normalize.css'
 import 'material-icons/iconfont/filled.css';
 import './style.css'
 
+
 const menuButton = document.querySelector('.menu-button')
 const closeButton = document.querySelector('.close-button')
 const header = document.querySelector('#header')
 const navLinks = document.querySelectorAll('#header nav ul li a')
+const main = document.querySelector('main') as HTMLElement;
+
+
+// debounce
+const debounce = (func: () => void, wait: number) => {
+  let timeout: ReturnType<typeof setTimeout>;
+  return () => {
+    clearTimeout(timeout);
+    timeout = setTimeout(func, wait);
+  };
+};
 
 const openMenu = () => {
   header?.classList.add('open')
@@ -22,6 +34,36 @@ closeButton?.addEventListener('click', closeMenu)
 navLinks.forEach((link) => {
   link.addEventListener('click', closeMenu)
 })
+
+const createDebouncedScrollHandler = () => {
+  let lastScroll = 0;
+  const triggerPx = 50;
+  return debounce(() => {
+    let currentScroll = main.scrollTop;
+    if (currentScroll > lastScroll && currentScroll > triggerPx) {
+      header?.classList.add('minimized')
+    } else {
+      header?.classList.remove('minimized')
+    }
+    lastScroll = currentScroll;
+  }, 10)
+}
+
+const debouncedScroll = createDebouncedScrollHandler();
+main?.addEventListener('scroll', debouncedScroll)
+
+// const debouncedScroll = debounce(() => {
+//   // const triggerPx = 50;
+//   // header?.classList.toggle('minimized', main.scrollTop  > triggerPx || false)
+//   let currentScroll = main.scrollTop;
+//   if (currentScroll > lastScroll && currentScroll > 50) {
+//     header?.classList.add('minimized')
+//   } else {
+//     header?.classList.remove('minimized')
+//   }
+//   lastScroll = currentScroll;
+// }, 100);
+
 
 // import typescriptLogo from './typescript.svg'
 // import viteLogo from '/vite.svg'
